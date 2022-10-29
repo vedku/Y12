bc7756bcfe41fdd5dfebb57260fb4aa6a8c53284
vedku <57610056+vedku@users.noreply.github.com>
2022-10-29 23:21:39 +0800

    Create bmicalculator.py

---
+++ term1/learning_functions/bmicalculator.py
@@ -0,0 +1,11 @@
+def height():
+    height = input("How tall are you? (M):")
+    return height
+
+def weight():
+    weight = int(input("How much do you weigh? (kg):"))
+    return weight
+
+weight = weight()
+height = height()
+BMI = weight/ height * height
