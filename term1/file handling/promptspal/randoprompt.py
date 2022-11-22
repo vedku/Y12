d046b891560f86b2eb39842a2c437efb292a1c58
vedku <57610056+vedku@users.noreply.github.com>
2022-11-22 14:15:37 +0800

    Update randoprompt.py

--- term1/file handling/promptspal/randoprompt.py
+++ term1/file handling/promptspal/randoprompt.py
@@ -1,5 +1,7 @@
 import random
 with open('promptspal.txt', 'r') as prompts:
     for line in prompts:
-        meow = line.split()
+        separator = "-Let's GO!"
+        meow = line.split(separator)
         print(random.choice(meow))
+        #print(meow)
